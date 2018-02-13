3077daee37ce15e68db8196317699a09bc870993
Achraf Dahmani <achraf.dahmani@hotmail.com>
2018-02-13 05:43:59 +0000

    basic infra & select/replace done

---
+++ spec/verbling-i18n-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import VerblingI18nView from '../lib/verbling-i18n-view';
+
+describe('VerblingI18nView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
